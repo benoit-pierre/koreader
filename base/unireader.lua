benoit-pierre/koreader

commit 2c058c6b1cbfc0731a03bcaca7a93aad4203dcac

add: goto input box in reader
Repo: base


Repo: base

base/unireader.lua
@@ -209,13 +209,17 @@ function UniReader:setzoom(page)
 	local dc = self.newDC()
 	local pwidth, pheight = page:getSize(self.nulldc)
 	print("# page::getSize "..pwidth.."*"..pheight);
-	local x0, y0, x1, y1 = page:getUsedBBox()
-	if x0 == 0.01 and y0 == 0.01 and x1 == -0.01 and y1 == -0.01 then
-		x0 = 0
-		y0 = 0
-		x1 = pwidth
-		y1 = pheight
+	local x0, y0, x1, y1 = 0, 0, pwidth, pheight
+
+	-- only get usedBBox in fit to content mode
+	if self.globalzoommode <= self.ZOOM_FIT_TO_CONTENT and
+	self.globalzoommode >= self.ZOOM_FIT_TO_CONTENT_HALF_WIDTH_MARGIN then 
+		x0, y0, x1, y1 = page:getUsedBBox()
+		if x0 == 0.01 and y0 == 0.01 and x1 == -0.01 and y1 == -0.01 then
+			x0, y0, x1, y1 = 0, 0, pwidth, pheight
+		end
 	end
+
 	-- clamp to page BBox
 	if x0 < 0 then x0 = 0 end
 	if x1 > pwidth then x1 = pwidth end
@@ -735,6 +739,17 @@ function UniReader:inputloop()
 				else
 					self:setglobalzoommode(self.ZOOM_FIT_TO_CONTENT_HALF_WIDTH_MARGIN)
 				end
+			elseif ev.code == KEY_G then
+				local page = InputBox:input(height-100, 100, "Page:")
+				-- convert string to number
+				if not pcall(function () page = page + 0 end) then
+					page = self.pageno
+				else
+					if page < 1 or page > self.doc:getPages() then
+						page = self.pageno
+					end
+				end
+				self:goto(page)
 			elseif ev.code == KEY_T then
 				self:showTOC()
 			elseif ev.code == KEY_B then

commit 375d82812f0ba49d19b42a88830ec2f493cc03db

revert 7f088d6159ac9b757a9063d91689c2c1e8c311a4 again
commit 2c058c6b1cbfc0731a03bcaca7a93aad4203dcac mistakenly
reverted 7a42851e68b92dcc2b4c0ab4bdd92ae59444964f, in which
commit 7f088d6159ac9b757a9063d91689c2c1e8c311a4 was reverted.

Repo: base


Repo: base

base/unireader.lua
@@ -209,17 +209,13 @@ function UniReader:setzoom(page)
 	local dc = self.newDC()
 	local pwidth, pheight = page:getSize(self.nulldc)
 	print("# page::getSize "..pwidth.."*"..pheight);
-	local x0, y0, x1, y1 = 0, 0, pwidth, pheight
-
-	-- only get usedBBox in fit to content mode
-	if self.globalzoommode <= self.ZOOM_FIT_TO_CONTENT and
-	self.globalzoommode >= self.ZOOM_FIT_TO_CONTENT_HALF_WIDTH_MARGIN then 
-		x0, y0, x1, y1 = page:getUsedBBox()
-		if x0 == 0.01 and y0 == 0.01 and x1 == -0.01 and y1 == -0.01 then
-			x0, y0, x1, y1 = 0, 0, pwidth, pheight
-		end
+	local x0, y0, x1, y1 = page:getUsedBBox()
+	if x0 == 0.01 and y0 == 0.01 and x1 == -0.01 and y1 == -0.01 then
+		x0 = 0
+		y0 = 0
+		x1 = pwidth
+		y1 = pheight
 	end
-
 	-- clamp to page BBox
 	if x0 < 0 then x0 = 0 end
 	if x1 > pwidth then x1 = pwidth end