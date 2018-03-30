benoit-pierre/koreader

commit 2352846381d72414f0a272340a5e6cdb2ba8fa98

[cblitbuffer] Supports screen inverse and rotation
Repo: base


Repo: base

base/ffi/framebuffer_SDL2_0.lua
@@ -22,6 +22,7 @@ function framebuffer:init()
         else
             self.bb = bb
         end
+        self.invert_bb = BB.new(SDL.w, SDL.h, BB.TYPE_BBRGB32)
 	else
 		self.bb = BB.new(600, 800)
     end
@@ -32,8 +33,13 @@ function framebuffer:init()
     framebuffer.parent.init(self)
 end
 
-local function render(bb)
-	SDL.SDL.SDL_UpdateTexture(SDL.texture, nil, bb.data, bb.pitch)
+function framebuffer:_render(bb)
+    if bb:getInverse() == 1 then
+        self.invert_bb:invertblitFrom(bb)
+	    SDL.SDL.SDL_UpdateTexture(SDL.texture, nil, self.invert_bb.data, self.invert_bb.pitch)
+    else
+	    SDL.SDL.SDL_UpdateTexture(SDL.texture, nil, bb.data, bb.pitch)
+    end
 	SDL.SDL.SDL_RenderClear(SDL.renderer)
 	SDL.SDL.SDL_RenderCopy(SDL.renderer, SDL.texture, nil, nil)
 	SDL.SDL.SDL_RenderPresent(SDL.renderer)
@@ -56,13 +62,13 @@ function framebuffer:refreshFullImp(x, y, w, h)
     local flash = os.getenv("EMULATE_READER_FLASH")
     if flash then
         self.sdl_bb:invertRect(x, y, w, h)
-        render(bb)
+        self:_render(bb)
         util.usleep(tonumber(flash)*1000)
         self.sdl_bb:setRotation(bb:getRotation())
         self.sdl_bb:setInverse(bb:getInverse())
         self.sdl_bb:blitFrom(bb, x, y, x, y, w, h)
     end
-    render(bb)
+    self:_render(bb)
 end
 
 function framebuffer:close()

base/Makefile
@@ -72,10 +72,10 @@ $(OUTPUT_DIR)/data:
 
 libs: \
 	$(if $(or $(SDL),$(ANDROID)),,$(OUTPUT_DIR)/libs/libkoreader-input.so) \
+	$(if $(or $(SDL),$(ANDROID)),$(OUTPUT_DIR)/libs/libblitbuffer.so,) \
 	$(OUTPUT_DIR)/libs/libkoreader-lfs.so \
 	$(OUTPUT_DIR)/libs/libkoreader-djvu.so \
 	$(OUTPUT_DIR)/libs/libkoreader-cre.so \
-	$(OUTPUT_DIR)/libs/libblitbuffer.so \
 	$(OUTPUT_DIR)/libs/libwrap-mupdf.so
 
 $(OUTPUT_DIR)/libs/libkoreader-input.so: input/*.c input/*.h $(if $(KINDLE),$(POPEN_NOSHELL_LIB),)

base/ffi/blitbuffer.lua
@@ -351,7 +351,7 @@ function Color4L_mt.__index:getColorRGB32()
 end
 Color4U_mt.__index.getColorRGB32 = Color4L_mt.__index.getColorRGB32
 Color8_mt.__index.getColorRGB32 = Color4L_mt.__index.getColorRGB32
-Color8A_mt.__index.getColorRGB32 = Color4L_mt.__index.getColorRGB32
+function Color8A_mt.__index:getColorRGB32() return ColorRGB32(self.a, self.a, self.a, self.alpha) end
 function ColorRGB16_mt.__index:getColorRGB32()
     local r = rshift(self.v, 11)
     local g = band(rshift(self.v, 5), 0x3F)
@@ -598,7 +598,7 @@ function BBRGB32_mt.__index.getMyColor(color) return color:getColorRGB32() end
 -- set pixel values
 function BB_mt.__index:setPixel(x, y, color)
     local px, py = self:getPhysicalCoordinates(x, y)
-    if self:getInverse() == 1 then color = color:invert() end
+    if not use_cblitbuffer and self:getInverse() == 1 then color = color:invert() end
     self:getPixelP(px, py)[0]:set(color)
 end
 function BB_mt.__index:setPixelAdd(x, y, color, alpha)
@@ -766,6 +766,11 @@ BB_mt.__index.blitFullFrom = BB_mt.__index.blitFrom
 
 -- blitting with a per-blit alpha value
 function BB_mt.__index:addblitFrom(source, dest_x, dest_y, offs_x, offs_y, width, height, intensity)
+    width, height = width or source:getWidth(), height or source:getHeight()
+    width, dest_x, offs_x = BB.checkBounds(width, dest_x or 0, offs_x or 0, self:getWidth(), source:getWidth())
+    height, dest_y, offs_y = BB.checkBounds(height, dest_y or 0, offs_y or 0, self:getHeight(), source:getHeight())
+    if width <= 0 or height <= 0 then return end
+
     if use_cblitbuffer then
         cblitbuffer.BB_add_blit_from(ffi.cast("struct BlitBuffer *", self),
             ffi.cast("struct BlitBuffer *", source),
@@ -777,6 +782,11 @@ end
 
 -- alpha-pane aware blitting
 function BB_mt.__index:alphablitFrom(source, dest_x, dest_y, offs_x, offs_y, width, height)
+    width, height = width or source:getWidth(), height or source:getHeight()
+    width, dest_x, offs_x = BB.checkBounds(width, dest_x or 0, offs_x or 0, self:getWidth(), source:getWidth())
+    height, dest_y, offs_y = BB.checkBounds(height, dest_y or 0, offs_y or 0, self:getHeight(), source:getHeight())
+    if width <= 0 or height <= 0 then return end
+
     if use_cblitbuffer then
         cblitbuffer.BB_alpha_blit_from(ffi.cast("struct BlitBuffer *", self),
             ffi.cast("struct BlitBuffer *", source),
@@ -788,6 +798,11 @@ end
 
 -- invert blitting
 function BB_mt.__index:invertblitFrom(source, dest_x, dest_y, offs_x, offs_y, width, height)
+    width, height = width or source:getWidth(), height or source:getHeight()
+    width, dest_x, offs_x = BB.checkBounds(width, dest_x or 0, offs_x or 0, self:getWidth(), source:getWidth())
+    height, dest_y, offs_y = BB.checkBounds(height, dest_y or 0, offs_y or 0, self:getHeight(), source:getHeight())
+    if width <= 0 or height <= 0 then return end
+
     if use_cblitbuffer then
         cblitbuffer.BB_invert_blit_from(ffi.cast("struct BlitBuffer *", self),
             ffi.cast("struct BlitBuffer *", source),
@@ -801,12 +816,12 @@ end
 function BB_mt.__index:colorblitFrom(source, dest_x, dest_y, offs_x, offs_y, width, height, color)
     -- we need color with alpha later:
     color = color:getColorRGB32()
-    if self:getInverse() == 1 then color = color:invert() end
     if use_cblitbuffer then
         cblitbuffer.BB_color_blit_from(ffi.cast("struct BlitBuffer *", self),
             ffi.cast("struct BlitBuffer *", source),
             dest_x, dest_y, offs_x, offs_y, width, height, color:getColorRGB32())
     else
+        if self:getInverse() == 1 then color = color:invert() end
         self:blitFrom(source, dest_x, dest_y, offs_x, offs_y, width, height, self.setPixelColorize, color)
     end
 end
@@ -1160,6 +1175,7 @@ dim color values in rectangular area
 function BB_mt.__index:dimRect(x, y, w, h, by)
     local color = Color8A(255, 255*(by or 0.5))
     if use_cblitbuffer then
+        c = color:getColorRGB32()
         cblitbuffer.BB_blend_rect(ffi.cast("struct BlitBuffer *", self),
             x, y, w, h, color:getColorRGB32())
     else

base/ffi/framebuffer_android.lua
@@ -46,7 +46,11 @@ function framebuffer:refreshFullImp()
         -- adapt to possible rotation changes
         bb:setRotation(ext_bb:getRotation())
 
-        bb:blitFrom(ext_bb)
+        if ext_bb:getInverse() == 1 then
+            bb:invertblitFrom(ext_bb)
+        else
+            bb:blitFrom(ext_bb)
+        end
     end
 
     android.lib.ANativeWindow_unlockAndPost(android.app.window);

commit 38b8aee0f8caef3f94e98758c6fa0fc5297f6482

[feat] SDL2: update parts of window like a real device (koreader/koreader-base#626)
This commit uses a viewport to draw only a subset of the blitbuffer onto the
rectangle to be updated in the SDL texture.

Without this, there are cases where a full screen (or at least bigger) refresh
is required that will not be obvious without first testing on a real device.
See https://github.com/koreader/koreader/pull/3804

Also fixes `EMULATE_READER_FLASH`, which was accidentally broken in 2352846381d72414f0a272340a5e6cdb2ba8fa98

Repo: base

base/ffi/SDL2_0.lua
@@ -80,6 +80,11 @@ function S.destroyTexture(texture)
     SDL.SDL_DestroyTexture(texture)
 end
 
+local rect = ffi.metatype("SDL_Rect", {})
+function S.rect(x, y, w, h)
+    return rect(x, y, w, h)
+end
+
 -- one SDL event can generate more than one event for koreader,
 -- so this represents a FIFO queue
 local inputQueue = {}

base/ffi/SDL2_0_h.lua
@@ -606,7 +606,12 @@ static const int SDL_WINDOWPOS_UNDEFINED = 536805376;
 static const int SDL_WINDOW_FULLSCREEN = 1;
 static const int SDL_WINDOW_FULLSCREEN_DESKTOP = 4097;
 static const int SDL_WINDOW_RESIZABLE = 32;
-static const int SDL_TEXTUREACCESS_STREAMING = 1;
+typedef enum
+{
+    SDL_TEXTUREACCESS_STATIC,
+    SDL_TEXTUREACCESS_STREAMING,
+    SDL_TEXTUREACCESS_TARGET
+} SDL_TextureAccess;
 static const int SDL_PIXELFORMAT_ARGB8888 = 372645892;
 static const int SDL_PIXELFORMAT_RGBA8888 = 373694468;
 static const int SDL_PIXELFORMAT_ABGR8888 = 376840196;

base/ffi/framebuffer_SDL2_0.lua
@@ -28,6 +28,8 @@ function framebuffer:resize(w, h)
 
     if not self.dummy then
         self:_newBB(w, h)
+        SDL.w = w
+        SDL.h = h
     else
         self.bb:free()
         self.bb = BB.new(600, 800)
@@ -72,13 +74,23 @@ function framebuffer:_newBB(w, h)
     end
 end
 
-function framebuffer:_render(bb)
+function framebuffer:_render(bb, x, y, w, h)
+    w, x = BB.checkBounds(w or bb:getWidth(), x or 0, 0, bb:getWidth(), 0xFFFF)
+    h, y = BB.checkBounds(h or bb:getHeight(), y or 0, 0, bb:getHeight(), 0xFFFF)
+    x, y, w, h = bb:getPhysicalRect(x, y, w, h)
+
     if bb:getInverse() == 1 then
         self.invert_bb:invertblitFrom(bb)
-        SDL.SDL.SDL_UpdateTexture(SDL.texture, nil, self.invert_bb.data, self.invert_bb.pitch)
-    else
-        SDL.SDL.SDL_UpdateTexture(SDL.texture, nil, bb.data, bb.pitch)
+        bb = self.invert_bb
     end
+
+    -- A viewport is a Blitbuffer object that works on a rectangular
+    -- subset of the underlying memory without allocating new memory.
+    local bb_rect = bb:viewport(x, y, w, h)
+    local sdl_rect = SDL.rect(x, y, w, h)
+
+    SDL.SDL.SDL_UpdateTexture(SDL.texture, sdl_rect, bb_rect.data, bb_rect.pitch)
+
     SDL.SDL.SDL_RenderClear(SDL.renderer)
     SDL.SDL.SDL_RenderCopy(SDL.renderer, SDL.texture, nil, nil)
     SDL.SDL.SDL_RenderPresent(SDL.renderer)
@@ -101,13 +113,13 @@ function framebuffer:refreshFullImp(x, y, w, h)
     local flash = os.getenv("EMULATE_READER_FLASH")
     if flash then
         self.sdl_bb:invertRect(x, y, w, h)
-        self:_render(bb)
+        self:_render(self.sdl_bb, x, y, w, h)
         util.usleep(tonumber(flash)*1000)
         self.sdl_bb:setRotation(bb:getRotation())
         self.sdl_bb:setInverse(bb:getInverse())
         self.sdl_bb:blitFrom(bb, x, y, x, y, w, h)
     end
-    self:_render(bb)
+    self:_render(bb, x, y, w, h)
 end
 
 function framebuffer:setWindowTitle(new_title)