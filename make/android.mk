a7df57e5e166621fde61c53f4cbe1465db0a7a65
Benoit Pierre <benoit.pierre@gmail.com>
2024-06-14 01:09:39 +0200

    android: compile luajit-laucher native code ourselves

--- make/android.mk
+++ make/android.mk
@@ -3,9 +3,11 @@ ANDROID_VERSION ?= $(shell git rev-list --count HEAD)
 ANDROID_NAME ?= $(VERSION)
 ANDROID_DIR = $(PLATFORM_DIR)/android
 ANDROID_LAUNCHER_DIR = $(ANDROID_DIR)/luajit-launcher
-ANDROID_ASSETS = $(ANDROID_LAUNCHER_DIR)/assets/module
-ANDROID_LIBS_ROOT = $(ANDROID_LAUNCHER_DIR)/libs
+ANDROID_LAUNCHER_BUILD = $(INSTALL_DIR)/luajit-launcher
+ANDROID_ASSETS = $(ANDROID_LAUNCHER_BUILD)/assets/module
+ANDROID_LIBS_ROOT = $(ANDROID_LAUNCHER_BUILD)/libs
 ANDROID_LIBS_ABI = $(ANDROID_LIBS_ROOT)/$(ANDROID_ABI)
+ANDROID_FLAVOR ?= Rocks
 ANDROID_APK = koreader-android-$(ANDROID_ARCH)$(KODEDUG_SUFFIX)-$(ANDROID_NAME).apk
 
 ifeq ($(ANDROID_ARCH), arm64)
@@ -19,6 +21,11 @@ else
   ANDROID_ABI ?= armeabi-v7a
 endif
 
+ifneq (,$(CI))
+  GRADLE_FLAGS ?= --console=plain --no-daemon -x lintVitalArmRocksRelease
+endif
+GRADLE_FLAGS += $(PARALLEL_JOBS:%=--max-workers=%)
+
 androiddev: update
 	$(MAKE) -C $(ANDROID_LAUNCHER_DIR) dev
 
@@ -29,14 +36,17 @@ update: all
 	mkdir -p $(ANDROID_ASSETS) $(ANDROID_LIBS_ABI)
 	# APK version
 	echo $(VERSION) > $(ANDROID_ASSETS)/version.txt
-	# shared libraries are stored as raw assets
-	cp -pR $(INSTALL_DIR)/koreader/libs $(ANDROID_LAUNCHER_DIR)/assets
-	# in runtime luajit-launcher's libluajit.so will be loaded
-	rm -vf $(ANDROID_LAUNCHER_DIR)/assets/libs/libluajit.so
+	# NOTE: we need to strip the version of library
+	# filenames, or they won't be added to the APK.
+	for src in $(INSTALL_DIR)/koreader/libs/*; do \
+	  dst="$${src##*/}"; \
+	  dst="$${dst%%.[0-9]*}"; \
+	  $(STRIP) --strip-unneeded "$$src" -o $(ANDROID_LIBS_ABI)/"$$dst"; \
+	done
 	# binaries are stored as shared libraries to prevent W^X exception on Android 10+
 	# https://developer.android.com/about/versions/10/behavior-changes-10#execute-permission
-	cp -pR $(INSTALL_DIR)/koreader/sdcv $(ANDROID_LIBS_ABI)/libsdcv.so
-	echo "sdcv libsdcv.so" > $(ANDROID_ASSETS)/map.txt
+	$(STRIP) --strip-unneeded $(INSTALL_DIR)/koreader/sdcv -o $(ANDROID_LIBS_ABI)/libsdcv.so
+	printf '%s\n' 'libs .' 'sdcv libsdcv.so' >$(ANDROID_ASSETS)/map.txt
 	# assets are compressed manually and stored inside the APK.
 	cd $(INSTALL_DIR)/koreader && \
 		./tools/mk7z.sh \
@@ -67,15 +77,27 @@ update: all
 		'-xr!README.md' \
 		;
 	# make the android APK
-	# Note: filter out the `--debug=…` make flag
-	# so the old crummy version provided by the
-	# NDK does not blow a gasket.
-	MAKEFLAGS='$(filter-out --debug=%,$(MAKEFLAGS))' \
-		$(MAKE) -C $(ANDROID_LAUNCHER_DIR) $(if $(KODEBUG), debug, release) \
-		ANDROID_APPNAME=KOReader \
-		ANDROID_VERSION=$(ANDROID_VERSION) \
-		ANDROID_NAME=$(ANDROID_NAME) \
-		ANDROID_FLAVOR=$(ANDROID_FLAVOR)
-	cp $(ANDROID_LAUNCHER_DIR)/bin/NativeActivity.apk $(ANDROID_APK)
+	env \
+		ANDROID_ARCH='$(ANDROID_ARCH)' \
+		ANDROID_ABI='$(ANDROID_ABI)' \
+		ANDROID_FULL_ARCH='$(ANDROID_ABI)' \
+		NDK=$(ANDROID_NDK_ROOT) \
+		SDK=$(ANDROID_SDK_ROOT) \
+		$(ANDROID_LAUNCHER_DIR)/gradlew \
+		--project-dir='$(abspath $(ANDROID_LAUNCHER_DIR))' \
+		--project-cache-dir='$(abspath $(ANDROID_LAUNCHER_BUILD)/gradle)' \
+		-PassetsPath='$(abspath $(dir $(ANDROID_ASSETS)))' \
+		-PbuildDir='$(abspath $(ANDROID_LAUNCHER_BUILD))' \
+		-PbuildJni=false \
+		-PlibsPath='$(abspath $(ANDROID_LIBS_ROOT))' \
+		-PndkCustomPath='$(ANDROID_NDK_ROOT)' \
+		-PprojectName='KOReader' \
+		-PversCode='$(ANDROID_VERSION)' \
+		-PversName='$(ANDROID_NAME)' \
+		$(GRADLE_FLAGS) \
+		'app:assemble$(ANDROID_ARCH)$(ANDROID_FLAVOR)$(if $(KODEBUG),Debug,Release)'
+	cp $(ANDROID_LAUNCHER_BUILD)/outputs/apk/$(ANDROID_ARCH)$(ANDROID_FLAVOR)/$(if $(KODEBUG),debug,release)/NativeActivity.apk $(ANDROID_APK)
+	# sign the APK with uber-apk-signer if it's available
+	$$(command -v uber-apk-signer true) --overwrite --apks $(ANDROID_APK)
 
 PHONY += androiddev update
