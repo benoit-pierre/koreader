5586f798c0c666054489b004ae750d53d1ae6eff
Benoit Pierre <benoit.pierre@gmail.com>
2024-10-05 14:57:16 +0200

    ci/macos: enable tests

--- make/macos.mk
+++ make/macos.mk
@@ -6,9 +6,10 @@ update: all
 		$(INSTALL_DIR)/bundle/Contents/Resources
 	cp -pv $(MACOS_DIR)/koreader.icns $(INSTALL_DIR)/bundle/Contents/Resources/icon.icns
 	cp -LR $(INSTALL_DIR)/koreader $(INSTALL_DIR)/bundle/Contents
+	rm -vf $(INSTALL_DIR)/bundle/Contents/koreader/luajit
 	cp -pRv $(MACOS_DIR)/menu.xml $(INSTALL_DIR)/bundle/Contents/MainMenu.xib
-	ibtool --compile "$(INSTALL_DIR)/bundle/Contents/Resources/Base.lproj/MainMenu.nib" "$(INSTALL_DIR)/bundle/Contents/MainMenu.xib"
-	rm -rfv "$(INSTALL_DIR)/bundle/Contents/MainMenu.xib"
+	ibtool --compile $(INSTALL_DIR)/bundle/Contents/Resources/Base.lproj/MainMenu.nib $(INSTALL_DIR)/bundle/Contents/MainMenu.xib
+	rm -vf $(INSTALL_DIR)/bundle/Contents/MainMenu.xib
 	$(CURDIR)/platform/mac/do_mac_bundle.sh $(INSTALL_DIR)
 
 PHONY += update
