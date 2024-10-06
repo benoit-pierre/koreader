6797ce7f874453cd00b32787c7265f6b94f31a12
Benoit Pierre <benoit.pierre@gmail.com>
2024-10-06 22:50:44 +0200

    tmp

--- make/macos.mk
+++ make/macos.mk
@@ -7,16 +7,15 @@ update: all
 	cp -pv $(MACOS_DIR)/koreader.icns $(INSTALL_DIR)/bundle/Contents/Resources/icon.icns
 	cd $(INSTALL_DIR)/koreader && find -L * \
 	  '(' \
-	     -name '.*' -o \
 	     -path './cache' -o \
 	     -path './clipboard' -o \
 	     -path './data/dict' -o \
 	     -path './data/tessdata' -o \
 	     -path './history' -o \
-	     -path './l10n/LICENSE' \
-	     -path './l10n/Makefile' \
-	     -path './l10n/README.md' \
-	     -path './l10n/templates' \
+	     -path './l10n/LICENSE' -o \
+	     -path './l10n/Makefile' -o \
+	     -path './l10n/README.md' -o \
+	     -path './l10n/templates' -o \
 	     -path './luajit' -o \
 	     -path './ota' -o \
 	     -path './plugins/SSH.koplugin' -o \
@@ -28,6 +27,7 @@ update: all
 	     -path './screenshots' -o \
 	     -path './spec' -o \
 	     -path './tools' -o \
+	     -name '.*' \
 	  ')' -prune -o -type f -print0 | cpio -Ldpm0 --quiet ../bundle/Contents/
 	cp -pRv $(MACOS_DIR)/menu.xml $(INSTALL_DIR)/bundle/Contents/MainMenu.xib
 	ibtool --compile $(INSTALL_DIR)/bundle/Contents/Resources/Base.lproj/MainMenu.nib $(INSTALL_DIR)/bundle/Contents/MainMenu.xib
