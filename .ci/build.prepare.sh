07345f9c1f60525812a9c50e018034b1f92bb919
Benoit Pierre <benoit.pierre@gmail.com>
2025-10-17 04:47:38 +0200

    ci: drop Circle CI in favor of Github Actions

---
+++ .ci/build.prepare.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -eo pipefail
+
+[[ $# -ge 1 ]]
+
+jobs_file="$1"
+shift 1
+
+yq=("$(which yq4 || which yq)") 2>/dev/null
+if "${yq[@]}" --yaml-fix-merge-anchor-to-spec 2>/dev/null; then
+    yq+=(--yaml-fix-merge-anchor-to-spec)
+fi
+
+jobs_rx="$("${yq[@]}" --output-format=props '"^(" + join("|") + ")$"' <<<"[$(printf '%s,' "$@")]")"
+printf 'jobs regex: %s\n' "${jobs_rx}" 1>&2
+
+all_jobs="$("${yq[@]}" --output-format=json . "${jobs_file}")"
+for variant in lint emulator macos platform; do
+    jobs="$(jq --arg variant "${variant}" --arg enabled "${jobs_rx}" --compact-output '
+    [.[$variant][] | select(.id | test($enabled)) |
+        .cache=(.cache // .id) | .target=(.target // .id)
+    ]' <<<"${all_jobs}")"
+    {
+        printf '%s jobs: ' "${variant}"
+        jq --color-output --sort-keys <<<"${jobs}"
+    } 1>&2
+    [[ "${jobs}" != '[]' ]] || jobs=''
+    printf '%s=%s\n' "${variant}" "${jobs}"
+done
+
+# vim: sw=4
