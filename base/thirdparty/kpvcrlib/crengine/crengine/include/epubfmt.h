4f95089afc09e7ecb4d30cfb60d7ff249e1c64ae
Benoit Pierre <benoit.pierre@gmail.com>
2026-01-31 23:13:00 +0100

    crengine: support calculating the raw text size / length

--- base/thirdparty/kpvcrlib/crengine/crengine/include/epubfmt.h
+++ base/thirdparty/kpvcrlib/crengine/crengine/include/epubfmt.h
@@ -6,7 +6,7 @@
 
 bool DetectEpubFormat( LVStreamRef stream );
 bool ImportEpubDocument( LVStreamRef stream, ldomDocument * doc, LVDocViewCallback * progressCallback,
-    CacheLoadingCallback * formatCallback, bool metadataOnly = false,
+    CacheLoadingCallback * formatCallback, int metadataOnly = 0,
     const elem_def_t * node_scheme=NULL, const attr_def_t * attr_scheme=NULL, const ns_def_t * ns_scheme=NULL);
 lString32 EpubGetRootFilePath( LVContainerRef m_arc );
 LVStreamRef GetEpubCoverpage(LVContainerRef arc);
