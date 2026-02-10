ac1a52e479c1003b5bb0d4ecee458aa90c582db5
Benoit Pierre <benoit.pierre@gmail.com>
2026-02-10 01:48:31 +0100

    only build new OTA artifacts

--- make/sony-prstux.mk
+++ make/sony-prstux.mk
@@ -1,7 +1,5 @@
 SONY_PRSTUX_DIR = $(PLATFORM_DIR)/sony-prstux
-SONY_PRSTUX_PACKAGE = koreader-sony-prstux$(KODEDUG_SUFFIX)-$(VERSION).zip
 SONY_PRSTUX_PACKAGE_OTA = koreader-sony-prstux$(KODEDUG_SUFFIX)-$(VERSION).tar.xz
-SONY_PRSTUX_PACKAGE_OLD_OTA = koreader-sony-prstux$(KODEDUG_SUFFIX)-$(VERSION).targz
 
 define UPDATE_PATH_EXCLUDES +=
 plugins/SSH.koplugin
@@ -14,8 +12,6 @@ update: all
 	# Sony PRSTUX launching scripts
 	$(SYMLINK) $(SONY_PRSTUX_DIR)/*.sh $(INSTALL_DIR)/koreader
 	# Create packages.
-	$(strip $(call mkupdate,$(SONY_PRSTUX_PACKAGE)))
 	$(strip $(call mkupdate,$(SONY_PRSTUX_PACKAGE_OTA)))
-	$(strip $(call mkupdate,$(SONY_PRSTUX_PACKAGE_OLD_OTA)))
 
 PHONY += update
