dfa94e457ed273200db53009b50a0585b64347d9
Benoit Pierre <benoit.pierre@gmail.com>
2026-03-01 20:19:42 +0100

    only build new OTA artifacts

--- make/pocketbook.mk
+++ make/pocketbook.mk
@@ -1,7 +1,5 @@
 POCKETBOOK_DIR = $(PLATFORM_DIR)/pocketbook
-PB_PACKAGE = koreader-pocketbook$(KODEDUG_SUFFIX)-$(VERSION).zip
 PB_PACKAGE_OTA = koreader-pocketbook$(KODEDUG_SUFFIX)-$(VERSION).tar.xz
-PB_PACKAGE_OLD_OTA = koreader-pocketbook$(KODEDUG_SUFFIX)-$(VERSION).targz
 
 define UPDATE_PATH_EXCLUDES +=
 tools
@@ -18,8 +16,6 @@ update: all
 	$(SYMLINK) $(POCKETBOOK_DIR)/system_koreader.app $(INSTALL_DIR)/system/bin/koreader.app
 	$(SYMLINK) $(INSTALL_DIR)/koreader $(INSTALL_DIR)/applications/
 	# Create packages.
-	$(strip $(call mkupdate,--manifest-transform=/^system\//d $(PB_PACKAGE),applications/koreader)) applications system
 	$(strip $(call mkupdate,--manifest-transform=/^system\//d $(PB_PACKAGE_OTA),applications/koreader)) applications system
-	$(strip $(call mkupdate,--manifest-transform=s/^/..\// $(PB_PACKAGE_OLD_OTA),applications/koreader)) applications
 
 PHONY += update
