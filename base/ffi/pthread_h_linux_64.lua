e42fe0dd4e9bd618925253414ce6f1bc25862c11
Benoit Pierre <benoit.pierre@gmail.com>
2026-04-25 00:55:41 +0200

    [TMP] generated ffi/phtread cdecls

---
+++ base/ffi/pthread_h_linux_64.lua
@@ -0,0 +1,17 @@
+-- Automatically generated with ffi-cdecl.
+
+local ffi = require("ffi")
+
+ffi.cdef[[
+static const int PTHREAD_CREATE_DETACHED = 1;
+union pthread_attr_t {
+  char __size[56];
+  long int __align;
+};
+typedef union pthread_attr_t pthread_attr_t;
+typedef long unsigned int pthread_t;
+int pthread_attr_destroy(pthread_attr_t *);
+int pthread_attr_init(pthread_attr_t *);
+int pthread_attr_setdetachstate(pthread_attr_t *, int);
+int pthread_create(pthread_t *restrict, const pthread_attr_t *restrict, void *(*)(void *), void *restrict);
+]]
