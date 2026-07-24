f55fd0380096c5ce9a8ebadebad06f49108ae3f3
Benoit Pierre <benoit.pierre@gmail.com>
2026-07-24 15:04:09 +0200

    zlib: switch to zlib-ng, version 2.3.3

--- base/ffi/zlib_h.lua
+++ base/ffi/zlib_h.lua
@@ -1,13 +1,8 @@
 -- Automatically generated with ffi-cdecl.
 
 require("ffi").cdef[[
-typedef unsigned char Byte;
-typedef Byte Bytef;
-typedef unsigned long uLong;
-typedef uLong uLongf;
-typedef unsigned uInt;
-uLong compressBound(uLong);
-int compress2(Bytef *, uLongf *, const Bytef *, uLong, int);
-uLong crc32(uLong, const Bytef *, uInt);
-int uncompress(Bytef *, uLongf *, const Bytef *, uLong);
+unsigned long compressBound(unsigned long);
+int compress2(unsigned char *, unsigned long *, const unsigned char *, unsigned long, int);
+unsigned long crc32(unsigned long, const unsigned char *, unsigned);
+int uncompress(unsigned char *, unsigned long *, const unsigned char *, unsigned long);
 ]]
