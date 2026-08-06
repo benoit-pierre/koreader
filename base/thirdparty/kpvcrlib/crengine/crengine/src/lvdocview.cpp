2765ed8bb2569546e3a2b82830e048cd03fd2c45
Benoit Pierre <benoit.pierre@gmail.com>
2026-08-06 03:15:23 +0200

    use `concat32` helper
    Repo: crengine

--- base/thirdparty/kpvcrlib/crengine/crengine/src/lvdocview.cpp
+++ base/thirdparty/kpvcrlib/crengine/crengine/src/lvdocview.cpp
@@ -3478,7 +3478,7 @@ bool LVDocView::goLink(lString32 link, bool savePos) {
 			// TODO: setup properties
 			// go to anchor
 			if (!id.empty())
-                goLink(cs32("#") + id);
+                goLink(concat32('#', id));
 			clearImageCache();
 			requestRender();
 			return true;
@@ -3528,7 +3528,7 @@ lString32 LVDocView::getNavigationPath() {
 	LVAppendPathDelimiter(fpath);
 	lString32 s = fpath + fname;
 	if (!m_arc.isNull())
-        s = cs32("/") + s;
+        s = concat32('/', s);
 	return s;
 }
 
