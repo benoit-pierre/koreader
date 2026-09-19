a0d5e0e9508508add287898db9f0fb142416802f
Benoit Pierre <benoit.pierre@gmail.com>
2026-09-19 12:29:56 +0200

    use `concat32` helper
    Repo: crengine

--- base/thirdparty/kpvcrlib/crengine/crengine/src/epubfmt.cpp
+++ base/thirdparty/kpvcrlib/crengine/crengine/src/epubfmt.cpp
@@ -874,7 +874,7 @@ public:
         if (uri[0] == U'/')
             _encrypted_items.set(uri.substr(1), encryption_method);
         else
-            _encrypted_items.set("/"+uri, encryption_method);
+            _encrypted_items.set(concat32('/', uri), encryption_method);
     }
 
     bool setAdobeManglingKey(lString32 key) {
@@ -1830,7 +1830,7 @@ bool ImportEpubDocument( LVStreamRef stream, ldomDocument * m_doc, LVDocViewCall
             // ldomDocumentFragmentWriter will get all id=, href=, src=... prefixed
             // with _doc_fragment_n, so they are unique in this single DOM.
             lString32 name = LVCombinePaths(codeBase, spineItems[i]->href);
-            lString32 subst = cs32("_doc_fragment_") + fmt::decimal(i);
+            lString32 subst = concat32("_doc_fragment_", fmt::decimal(i));
             appender.addPathSubstitution( name, subst );
             //CRLog::trace("subst: %s => %s", LCSTR(name), LCSTR(subst));
         }
